033646a6c0c0fda1ef6e5a90b295b2c70fa7d21d
Lindsay-R <lrecknell@gmail.com>
2014-06-23 16:33:57 -0600

    end of class Mon.

--- lib/dogs.rb
+++ lib/dogs.rb
@@ -30,4 +30,73 @@ class Dogs
 
   # only edit below this line
 
-end
+  def small_dogs
+    small_dog_array = []
+    @dogs.each do |dog|
+      small_dog_array.push(dog) if dog.has_value?(:small)
+    end
+    small_dog_array
+  end
+
+  def huge_dog
+    huge_dog_array = []
+    @dogs.each do |dog|
+      huge_dog_array.push(dog) if dog.has_value?(:huge)
+    end
+    huge_dog_array[0]
+  end
+
+  def large_dog_names
+    large_dog_name_array = []
+    @dogs.each do |dog|
+      large_dog_name_array.push(dog) if dog.has_value?(:large)
+    end
+    name_array = []
+    large_dog_name_array.each do |name|
+      name_array.push(name[:name])
+    end
+    name_array
+  end
+
+  def joes_large_dogs
+    joes_large_dogs_array = []
+
+    @dogs.each do |dog|
+      if dog[:owner][:name][:first] =="Joe" && dog[:size] == :large
+        joes_large_dogs_array.push(dog[:name])
+      end
+    end
+    joes_large_dogs_array
+  end
+
+  def sizes
+    size_array = []
+    @dogs.each do |dog|
+      size_array.push(dog[:size])
+      size_array.uniq!
+    end
+    size_array
+  end
+
+  def owners
+    first_array = []
+    @dogs.each do |dog|
+      first_array.push(dog[:owner][:name][:first])
+      first_array.uniq!
+
+    end
+    print first_array
+
+    last_array = []
+    @dogs.each do |dog|
+      last_array.push(dog[:owner][:name][:last])
+      last_array.uniq!
+
+    end
+    print last_array
+
+
+  end
+
+
+end #end of class
